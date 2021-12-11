d5b2caa8019512d49d823e5c110d78465bb2900d
Muhammad Jawad <95573894+Jawad440@users.noreply.github.com>
2021-12-10 21:46:48 -0800

    Update 11813.cpp

--- 11813.cpp
+++ 11813.cpp
@@ -1,7 +1,13 @@
+/*program which take two input from user and multiply these value */
 #include <iostream>
 using namespace std;
 
 main()
 {
-	cout<<"\n********( Hello World )************"<<endl;
+	int a; cout<<"Enter A value : "; cin>>a;
+	int b; cout<<"Enter B value : "; cin>>b;
+	
+	int result = (a*b);
+	
+	cout<<"Multiplication of "<<a<<" and  "<<b<<" : "<<result<<endl;
 }
